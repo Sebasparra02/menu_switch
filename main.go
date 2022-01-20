962f49b58471f3e35a85a5e94a35e4b2bde6464b
Sebasparra02 <parrasebastian954@gmail.com>
2022-01-20 18:27:24 -0500

    “mensaje_de_mis_cambios”

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("***MENU PRINCIPAL***")
 	menu_principal := `
+	***MENU PRINCIPAL***
 	1) Cambiar su contraseña
 	2) Editar perfil 
 	3) Foro
@@ -14,15 +14,54 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println("***MENU PERFIL***")
 	menu_perfil := `
+	***MENU PERFIL***
 	1) Cambiar foto de perfil
 	2) Cambiar nombre de usuario
 	3) Editar biografia
 	4) Volver al menu principal`
-	_, err := fmt.Scanf("%s", &menu_perfil)
+	_, err = fmt.Scanf("%s", &menu_perfil)
 	if err != nil {
 		panic(err)
+	}
+
+	menu_foro := `
+	***MENU FORO***
+	1) Publicar nueva entrada en el foro
+	2) Editar una entrada del foro
+	3) Eliminar una entrada del foro
+	4) Volver al menu principal`
+	_, err = fmt.Scanf("%s", &menu_foro)
+	if err != nil {
+		panic(err)
+	}
+
+	var menus string
+	var seguir string
+
+	for {
+		switch menus {
+		case "m.p":
+			fmt.Println(menu_principal)
+			_, err = fmt.Scanf("%s", &menu_principal)
+			if err != nil {
+				panic(err)
+			}
+
+			if menu_principal == "1" {
+				fmt.Println("Su contraseña a sido cambiada con exito")
+			}
+			fmt.Println("desea seguir navegando o desea finalizar el programa?")
+			fmt.Println("si desea seguir indique SI de lo contrario NO")
+			if seguir == "SI" {
+				fmt.Println(menu_principal)
+			} else {
+				fmt.Println("te esperamos pronto")
+				break
+			}
+
+		}
+
 	}
 
 }
