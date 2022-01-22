96171aa46a6be76a0a78390fd11aaea7fee985d8
Arianny Parra <ariannyparra@MacBook-Pro-de-ADMIN.local>
2022-01-21 19:40:51 -0500

    Arreglando el condicional switch

--- main.go
+++ main.go
@@ -9,10 +9,6 @@ func main() {
 	2) Editar perfil 
 	3) Foro
 	4) Finalizar programa`
-	_, err := fmt.Scanf("%s", &menu_principal)
-	if err != nil {
-		panic(err)
-	}
 
 	menu_perfil := `
 	***MENU PERFIL***
@@ -20,10 +16,6 @@ func main() {
 	2) Cambiar nombre de usuario
 	3) Editar biografia
 	4) Volver al menu principal`
-	_, err = fmt.Scanf("%s", &menu_perfil)
-	if err != nil {
-		panic(err)
-	}
 
 	menu_foro := `
 	***MENU FORO***
@@ -31,32 +23,21 @@ func main() {
 	2) Editar una entrada del foro
 	3) Eliminar una entrada del foro
 	4) Volver al menu principal`
-	_, err = fmt.Scanf("%s", &menu_foro)
-	if err != nil {
-		panic(err)
-	}
 
 	var seguir string
+	opcion_menu_principal := 0
 
 	for {
-		switch menu_principal {
-		case "m.p":
+		switch opcion_menu_principal {
+		case 0:
 			fmt.Println(menu_principal)
-			_, err = fmt.Scanf("%s", &menu_principal)
+			_, err := fmt.Scanf("%s", &opcion_menu_principal)
 			if err != nil {
 				panic(err)
 			}
 
-			if menu_principal == "1" {
+			if opcion_menu_principal == 1 {
 				fmt.Println("Su contraseña a sido cambiada con exito")
-			}
-			fmt.Println("desea seguir navegando o desea finalizar el programa?")
-			fmt.Println("si desea seguir indique SI de lo contrario NO")
-			if seguir == "SI" {
-				fmt.Println(menu_principal)
-			} else {
-				fmt.Println("te esperamos pronto")
-				break
 			}
 
 		}
